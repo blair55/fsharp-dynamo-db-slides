4ed217665ed723e05ba9a2d8d899a9883223808c
Blair55 <blair.nicholas@googlemail.com>
2020-02-12 02:12:41 +0000

    wip

---
+++ .paket/load/netcoreapp3.1/AWSSDK.SecurityToken.fsx
@@ -0,0 +1,4 @@
+namespace PaketLoadScripts
+
+#load "/Users/nblair/lab/fs-dbd-slides/.paket/load/netcoreapp3.1/AWSSDK.Core.fsx" 
+#r "/Users/nblair/.nuget/packages/awssdk.securitytoken/3.3.104.29/lib/netstandard2.0/AWSSDK.SecurityToken.dll" 
